c342ecd28bcf78ac812c1707663dc72d37966eac
Aydın <107302132+thisisaae@users.noreply.github.com>
2022-09-24 16:34:46 +0300

    Create hesapmakinesi.py

---
+++ hesapmakinesi.py
@@ -0,0 +1,47 @@
+print("""
+
+        +-------- Python Hesap Makinesi ----------------+
+        |                                               |
+        |    TOPLAMA İŞLEMİ İÇİN | 1                    |
+        |    ÇIKARMA İŞLEMİ İÇİN | 2                    |
+        |    ÇARPMA İŞLEMİ İÇİN  | 3                    |
+        |    BÖLME İŞLEMİ İÇİN   | 4                    |
+        +-----------------------------------------------+
+
+""")
+
+islem = str(input("İşlem seçiniz: "))
+
+if islem == "1":
+    sayi1 = int(input("sayi1 giriniz: "))
+    sayi2 = int(input("sayi2 giriniz: "))
+    print("Sonuç:", sayi1 + sayi2)
+elif islem == "2":
+    sayi1 = int(input("sayi1 giriniz: "))
+    sayi2 = int(input("sayi2 giriniz: "))
+    print("Sonuç:", sayi1 - sayi2)
+elif islem == "3":
+    sayi1 = int(input("sayi1 giriniz: "))
+    sayi2 = int(input("sayi2 giriniz: "))
+    print("Sonuç:", sayi1 * sayi2)
+elif islem == "4":
+    sayi1 = int(input("sayi1 giriniz: "))
+    sayi2 = int(input("sayi2 giriniz: "))
+    print("Sonuç:", sayi1/sayi2)
+else:
+    print("geçersiz işlem girdiniz...")
+
+'''
+░█████╗░██╗░░░██╗██████╗░██╗███╗░░██╗
+██╔══██╗╚██╗░██╔╝██╔══██╗██║████╗░██║
+███████║░╚████╔╝░██║░░██║██║██╔██╗██║
+██╔══██║░░╚██╔╝░░██║░░██║██║██║╚████║
+██║░░██║░░░██║░░░██████╔╝██║██║░╚███║
+╚═╝░░╚═╝░░░╚═╝░░░╚═════╝░╚═╝╚═╝░░╚══╝
+'''
+#Bilim gerçeği bilmektir - M. Kemal Atatürk
+'''
+Bana ulaşın:
+reddit : u/thisisae
+discord : tostos adam#9918
+'''
